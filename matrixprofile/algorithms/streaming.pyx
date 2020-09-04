e11cad2dcf0d59dcbcd3723634522a73cfe92a9c
kavj <kjkamgar@gmail.com>
2020-09-04 16:50:53 -0700

    Adding some support funcs

--- matrixprofile/algorithms/streaming.pyx
+++ matrixprofile/algorithms/streaming.pyx
@@ -1,7 +1,92 @@
 # -*- coding: utf-8 -*-
 #cython: boundscheck=False, cdivision=True, wraparound=False
 
+from libc.math cimport sqrt
 from matrixprofile.cycore import muinvn
+
+
+cdef windowed_mean(double [::1] ts, double[::1] mu): 
+    cdef Py_ssize_t tslen = ts.shape[0]
+    cdef Py_ssize_t windowlen = tslen - mu.shape[0] + 1
+    if tslen < windowlen:
+        raise ValueError, "Window length exceeds the number of elements in the time series"
+    cdef double accum = ts[0];
+    cdef double resid = 0;
+    cdef Py_ssize_t i
+    cdef double m, n, p, q, r, s
+    for i in range(1, windowlen):
+        m = ts[i]
+        p = accum
+        accum += m
+        q = accum - p
+        resid += ((p - (accum - q)) + (m - q))
+    mu[0] = (accum + resid) / windowlen
+    for i in range(windowlen, tslen):
+        m = ts[i - windowlen]
+        n = ts[i]
+        p = accum - m
+        q = p - accum
+        r = resid + ((accum - (p - q)) - (m + q))
+        accum = p + n
+        s = accum - p
+        resid = r + ((p - (accum - s)) + (n - s))
+        mu[i - windowlen + 1] = (accum + resid) / windowlen
+    return mu
+
+
+cdef windowed_cnorm(double [::1] ts, double[::1] mu, double[::1] sig):
+    cdef Py_ssize_t tslen = ts.shape[0]
+    cdef Py_ssize_t windowcount = mu.shape[0]
+    if windowcount < 1 or windowcount > tslen: 
+        raise ValueError
+
+    cdef Py_ssize_t windowlen = tslen - windowcount + 1
+    cdef double accum = 0
+    cdef double m_
+    cdef Py_ssize_t i, j
+
+    for i in range(windowcount):
+        m_ = mu[i]
+        accum = 0
+        for j in range(i, i + windowlen):
+            accum += (ts[j] - m_)**2
+        sig[i] = sqrt(accum)
+    return sig
+
+
+# Todo: refactor all mpx logic and moment calculations to a sharable C interface that does not use numpy's namespace
+
+cdef normalize(double [:, ::1] ss, double[::1] ts, double[::1] mu, double[::1] sig, Py_ssize_t start):
+    cdef Py_ssize_t windowcount = ss.shape[0]
+    cdef Py_ssize_t windowlen = ss.shape[1]
+    windowcount = mu.shape[0]
+    windowlen = ts.shape[0] - mu.shape[0] + 1
+    # ts, mu, and sig must represent the same number of available subsequences
+    # start dictates the starting index within these, as this avoids redundant slicing of multiple components
+    # ss.shape[0] exposes the number of subsequences that should be normalized
+    # 
+
+    cdef Py_ssize_t i,j
+    cdef double m_, s_
+    for i in range(windowcount):
+        accum = 0.0
+        m_ = mu[i]
+        s_ = sig[i]
+        for j in range(windowlen):
+            ss[i, j] = (ts[i + j] - m_) / s_
+
+
+# factored out from mpx method, should probably go somewhere else
+cdef mpx_step_eqns(double [::1] ts, double [::1] mu, double[::1] mu_s, double[::1] rbwd, double[::1] cbwd, double[::1] rfwd, double[::1] cfwd):    
+    cdef Py_ssize_t sseqct = mu.shape[0]
+    cdef Py_ssize_t sseqlen = ts.shape[0] - mu.shape[0] + 1
+    cdef Py_ssize_t i
+
+    for i in range(sseqct-1):
+        rbwd[i] = ts[i] - mu[i]
+        cbwd[i] = ts[i] - mu_s[i+1]
+        rfwd[i] = ts[i+sseqlen] - mu[i+1]
+        cfwd[i] = ts[i+sseqlen] - mu_s[i+1]
 
 
 cdef class auto_params:
@@ -80,41 +165,41 @@ cdef class auto_params:
         cdef Py_ssize_t shiftby = drop_below - self.minidx
         if shiftby < 0:
             return
-        
-        cdef Py_ssize_t updf = self.rlen - shiftby
-        self._ts[:updf] = self._ts[shiftby:self.rlen]
-        self._mu[:updf] = self._ts[shiftby:self.rlen]
-        self._invn[:updf] = self._invn[shiftby:self.rlen]
-        self._r_bwd[:updf] = self._r_bwd[shiftby:self.rlen]
-        self._r_fwd[:updf] = self._r_fwd[shiftby:self.rlen]
-        self._c_bwd[:updf] = self._c_bwd[shiftby:self.rlen]
-        self._c_fwd[:updf] = self._c_fwd[shiftby:self.rlen]
-        self.rlen -= shiftby
+        cdef Py_ssize_t updf = self.rlen - shiftby if shiftby < self.rlen else 0
+        if updf < self.rlen:
+            self._ts[:updf] = self._ts[shiftby:self.rlen]
+            self._mu[:updf] = self._ts[shiftby:self.rlen]
+            self._invn[:updf] = self._invn[shiftby:self.rlen]
+            self._r_bwd[:updf] = self._r_bwd[shiftby:self.rlen]
+            self._r_fwd[:updf] = self._r_fwd[shiftby:self.rlen]
+            self._c_bwd[:updf] = self._c_bwd[shiftby:self.rlen]
+            self._c_fwd[:updf] = self._c_fwd[shiftby:self.rlen]
+        self.rlen = updf
 
     cdef append(self, double[::1] dat, Py_ssize_t drop_below=0):
-        cdef Py_ssize_t start = 0 if drop_below <= self.minidx else self.minidx - drop_below
-        cdef Py_ssize_t updsz = self.rlen - start + dat.shape[0]
+        cdef Py_ssize_t startpos = 0 if drop_below <= self.minidx else self.minidx - drop_below
+        cdef Py_ssize_t updsz = self.rlen - startpos + dat.shape[0]
         if updsz > self.ts.shape[0]:
-            self.resize(2*self._ts.shape[0] if updsz <= 2*self._ts.shape[0] else 2*updsz)
-            self.rlen -= start        
+            self.resize(2*self._ts.shape[0] if updsz <= 2*self._ts.shape[0] else 2*updsz, drop_below)
+        # append here
+
 
 cdef class mpstream:
 
-    def __cinit__(self, Py_ssize_t sseqlen, Py_ssize_t minlag, Py_ssize_t maxlag, Py_ssize_t init_buffer_len=4096):
+    def __cinit__(self, Py_ssize_t sseqlen, Py_ssize_t minsep, Py_ssize_t maxsep, Py_ssize_t init_buffer_len=4096):
         if sseqlen < 4:
             raise ValueError
-        if minlag <= 0:
-            raise ValueError
-        elif maxlag <= 0:  # should have some default
-            raise ValueError
+        if minsep <= 0:
+            raise ValueError(f'minsep must be strictly positive, received a value of {minsep}')
+        elif maxsep <= 0 or maxsep <= minsep:  # should have some default
+            raise ValueError('max separation must be positive and strictly larger than minsep, received minsep:{minsep}, maxsep:{maxep}')
         elif init_buffer_len <= 0:
             raise ValueError
-        self.minlag = minlag
-        self.maxlag = maxlag
+        self.minsep = minsep
+        self.maxsep = maxsep
         self.profilelen = 0
  
     cdef append(self, auto_params sect):
         pass
  
-        
 
