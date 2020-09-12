eb8796a5f2ad141b2d3152e3ab40d3b6c850c82a
kavj <kjkamgar@gmail.com>
2020-09-12 08:48:46 -0700

    Change to fixed buffer size

--- matrixprofile/algorithms/streaming.pxd
+++ matrixprofile/algorithms/streaming.pxd
@@ -10,39 +10,33 @@ from cython.view cimport array
 cdef windowed_mean(double [::1] ts, double[::1] mu)
 cdef windowed_invcent_norm(double[::1] ts, double[::1] mu, double[::1] sig)
 cdef normalize_one(double[::1] out, double[::1] ts, double mu, double sig)
-cdef normalize_one(double[::1] out, double[::1] ts, double mu, double sig)
 cdef crosscov(double[::1] out, double[::1] ts, double[::1] mu, double[::1] sig, double[::1] cmpseq)
-cdef mpx_step_eqns(double[::1] ts, double[::1] mu, double[::1] mu_s, double[::1] rbwd, double[::1] cbwd, double[::1] rfwd, double[::1] cfwd)
 
 
-cdef class AutoParams:
+cdef class TSParams:
     cdef:
-        array ts
-        array mu
-        array invn
-        array r_bwd
-        array c_bwd
-        array r_fwd
-        array c_fwd
+        array _ts
+        array _mu
+        array _invn
+        inline double[::1] ts(self)
+        inline double[::1] mu(self)
+        inline double[::1] invn(self)
+        inline Py_ssize_t buffered_len(self)
+        inline Py_ssize_t buffered_subseqct(self)
+        repack(self, Py_ssize_t dropct=?)
         append(self, double[::1] dat, Py_ssize_t dropct=?)
-        resize(self, Py_ssize_t sz, Py_ssize_t dropct=?)
-        reserve(self, Py_ssize_t sz)
-        repack(self, Py_ssize_t dropct)
-        inline Py_ssize_t last_abs_idx(self)
-        inline Py_ssize_t sseqct(self)
      
     cdef readonly:
-        Py_ssize_t minidx                         # first index of the in memory portion
-        inline Py_ssize_t signal_len(self)        # total length of the signal at this point, including dropped sections
-        Py_ssize_t tslen                          # number of time series elements currently buffered in memory
-        inline Py_ssize_t signal_sseqct(self)
-        row_diffs(self, Py_ssize_t begin=?)
-        col_diffs(self, Py_ssize_t begin=?)
+        inline Py_ssize_t maxindex(self)
+        inline Py_ssize_t maxsubseqindex(self)
+        Py_ssize_t minindex                      
+        inline Py_ssize_t buffered_len(self)        
+        inline Py_ssize_t buffered_subseqct(self)
   
 
-cdef class AutoMProfile:
+cdef class MProfile:
     cdef:
-        AutoParams tsp
+        TSParams ts
         array _mp
         array _mpi
         append(self, double[::1] ts)
--- matrixprofile/algorithms/streaming.pyx
+++ matrixprofile/algorithms/streaming.pyx
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #cython: boundscheck=False, cdivision=True, wraparound=False
-
+from cython.view cimport array
 from libc.math cimport sqrt
 from matrixprofile.cycore import muinvn
 
@@ -79,178 +79,93 @@ cdef crosscov(double[::1] out, double[::1] ts, double[::1] mu, double[::1] sig, 
         out[i] = accum
 
 
-# factored out from mpx method, should probably go somewhere else
-cdef mpx_step_eqns(double[::1] ts, double[::1] mu, double[::1] mu_s, double[::1] rbwd, double[::1] cbwd, double[::1] rfwd, double[::1] cfwd):    
-    cdef Py_ssize_t sseqct = mu.shape[0]
-    cdef Py_ssize_t sseqlen = ts.shape[0] - mu.shape[0] + 1
+cdef mpx_difeq(double [::1] out, double[::1] ts, double[::1] mu):
+    if not (ts.shape[0] == mu.shape[0] == out.shape[0]):
+        raise ValueError(f'time series of shape {ts.shape[0]} is incompatible with mean vector mu of shape {mu.shape[0]} and output shape {out.shape[0]}')
     cdef Py_ssize_t i
-
-    for i in range(sseqct-1):
-        rbwd[i] = ts[i] - mu[i]
-        cbwd[i] = ts[i] - mu_s[i+1]
-        rfwd[i] = ts[i+sseqlen] - mu[i+1]
-        cfwd[i] = ts[i+sseqlen] - mu_s[i+1]
+    for i in range(ts.shape[0]):
+        out[i] = ts[i] - mu[i]
 
 
-cdef class AutoParams:
+#    self.r_bwd = array(shape=(ss_buf_ct-1,), itemsize=eltz, format='d')
+#    self.c_bwd = array(shape=(ss_buf_ct-1,), itemsize=eltsz, format='d')
+#    self.r_fwd = array(shape=(ss_buf_ct-1,), itemsize=eltsz, format='d')
+#    self.c_fwd = array(shape=(ss_buf_ct-1,), itemsize=eltsz, format='d')
+#    self.tslen = ts.shape[0]
+#    self.sseqlen = sseqlen
+#    self.minidx = offset
+ 
+
+
+cdef class TSParams:
     """ Descriptor for time series matrix profile calculations using method mpx.
 
     """
 
-    def __cinit__(self, double[::1] ts, Py_ssize_t sseqlen, Py_ssize_t minreslen = 4096, Py_ssize_t offset=0):
-        cdef init_len
-        if minreslen >= 2 * ts.shape[0]:
-            init_len = minreslen
-        else:
-            init_len = 2 * ts.shape[0]
-        cdef ss_buf_ct = init_len - sseqlen + 1
-        self.ts = array(shape=(init_len,), itemsize=sizeof(double), format='d')
-        self.mu = array(shape=(ss_buf_ct,), itemsize=sizeof(double), format='d')
-        self.invn = array(shape=(ss_buf_ct,), itemsize=sizeof(double), format='d')
-        self.r_bwd = array(shape=(ss_buf_ct-1,), itemsize=sizeof(double), format='d')
-        self.c_bwd = array(shape=(ss_buf_ct-1,), itemsize=sizeof(double), format='d')
-        self.r_fwd = array(shape=(ss_buf_ct-1,), itemsize=sizeof(double), format='d')
-        self.c_fwd = array(shape=(ss_buf_ct-1,), itemsize=sizeof(double), format='d')
-        self.tslen = ts.shape[0]
-        self.sseqlen = sseqlen
-        self.minidx = offset
-        windowed_mean(self.ts, self.mu)
+    def __cinit__(self, Py_ssize_t subseqlen, Py_ssize_t subseqbuflen):
+        cdef Py_ssize_t eltsz = sizeof(double)
+        cdef Py_ssize_t tsbuflen = subseqbuflen + subseqlen - 1
+        self._ts = array(shape=(tsbuflen,), itemsize=eltsz, format='d')
+        self._mu = array(shape=(subseqbuflen,), itemsize=eltsz, format='d')
+        self._invn = array(shape=(subseqbuflen,), itemsize=eltsz, format='d')
+        self.subseqlen = subseqlen
+        self.minindex = 0
+        self.beginpos = 0
+        self.endpos = 0
 
+    cdef inline double[::1] ts(self):
+        return self._ts[self.beginpos:self.endpos]
 
-    cdef inline Py_ssize_t sseqct(self):
-        return self.tslen - self.sseqlen + 1 if self.tslen >= self.sseqlen else 0
+    cdef inline double[::1] mu(self):
+        return self._mu[self.beginpos:self.beginpos+self.buffered_subseqct()]
 
+    cdef inline double[::1] invn(self):
+        return self._invn[self.beginpos:self.beginpos+self.buffered_subseqct()]
 
-    cdef inline Py_ssize_t signal_len(self):
-        return self.minidx + self.tslen
+    cdef inline Py_ssize_t buffered_len(self):
+        return self.endpos - self.beginpos
 
+    cdef inline Py_ssize_t buffered_subseqct(self):
+        cdef Py_ssize_t bl = self.buffered_len()
+        return bl - self.sseqlen + 1 if bl > self.sseqlen else 0
 
-    cdef inline Py_ssize_t signal_sseqct(self):
-        return self.signal_len() - self.sseqlen + 1
+    cdef inline Py_ssize_t maxindex(self):
+        return self.minindex + self.tslen
 
+    cdef inline Py_ssize_t maxsubseqindex(self):
+        return self.minindex + self.subseqct()
 
-    cdef inline Py_ssize_t last_abs_idx(self):
-        return self.sseqlen + self.minidx 
-
-
-    cdef row_diffs(self, Py_ssize_t begin=0):
-        cdef Py_ssize_t endpos = self.sseqct()-1
-        if endpos < 0:
-            endpos = 0
-        return self.r_bwd[begin:endpos], self.r_fwd[begin:endpos]
-
-
-    cdef col_diffs(self, Py_ssize_t begin=-1):
-        cdef Py_ssize_t endpos = self.sseqct()-1
-        if endpos < 0:
-            endpos = 0
-        return self.c_bwd[begin:endpos], self.c_fwd[begin:endpos]
-
-
-    cdef resize(self, Py_ssize_t sz, Py_ssize_t dropct=0):
-        if dropct > self.tslen:
-            raise ValueError
-        elif self.tslen - dropct > sz:
-            raise ValueError
-        cdef Py_ssize_t ss_sz = sz - self.sseqlen + 1 if sz >= self.sseqlen else 0
-        cdef Py_ssize_t dif_sz = ss_sz - 1 if ss_sz > 0 else 0
-        cdef array ts = array(shape=(sz,), itemsize=sizeof(double), format='d')
-        cdef array mu = array(shape=(ss_sz,), itemsize=sizeof(double), format='d')
-        cdef array invn = array(shape=(ss_sz,), itemsize=sizeof(double), format='d')
-        cdef array rbwd = array(shape=(dif_sz,), itemsize=sizeof(double), format='d')
-        cdef array rfwd = array(shape=(dif_sz,), itemsize=sizeof(double), format='d')
-        cdef array cbwd = array(shape=(dif_sz,), itemsize=sizeof(double), format='d')
-        cdef array cfwd = array(shape=(dif_sz,), itemsize=sizeof(double), format='d')  
-        
-        cdef Py_ssize_t old_tslen = self.tslen        
-        cdef Py_ssize_t old_ssct = self.sseqct()
-
-        if dropct < old_tslen:
-            self.tslen -= dropct
-            ts[:self.tslen] = self.ts[dropct:old_tslen]
-        else:
-            self.tslen = 0
-        cdef Py_ssize_t ssct = self.sseqct()
-        if dropct < old_ssct:
-            mu[:ssct] = self.mu[dropct:old_ssct]
-            invn[:ssct] = self.invn[dropct:old_ssct]
-        if ssct > 1:
-            rbwd[:ssct-1] = self.r_bwd[dropct:old_ssct-1]
-            rfwd[:ssct-1] = self.r_fwd[dropct:old_ssct-1]
-            cbwd[:ssct-1] = self.c_bwd[dropct:old_ssct-1]
-            cfwd[:ssct-1] = self.c_fwd[dropct:old_ssct-1]
-        self.ts = ts
-        self.mu = mu
-        self.invn = invn
-        self.r_bwd = rbwd
-        self.r_fwd = rfwd
-        self.c_bwd = cbwd
-        self.c_fwd = cfwd
-      
-
-    cdef reserve(self, Py_ssize_t sz):
-        if self.ts.shape[0] < sz:
-            self.resize(sz)
-
-
-    cdef repack(self, Py_ssize_t dropct):
+    cdef repack(self, Py_ssize_t dropct=0):
         if dropct <= 0:
-           raise ValueError
-        elif dropct > self.tslen:
-            raise ValueError
-        cdef Py_ssize_t old_tslen = self.tslen
-        cdef Py_ssize_t old_ssct = self.sseqct()
-        self.tslen -= dropct
-        self.ts[:self.tslen] = self.ts[dropct:old_tslen]
-        cdef Py_ssize_t ssct = self.sseqct()
-        if ssct > 0:
-            self.mu[:ssct] = self.mu[dropct:old_ssct]
-            self.invn[:ssct] = self.invn[dropct:old_ssct]
-        if ssct > 1:
-            self.r_bwd[:ssct-1] = self.r_bwd[dropct:old_ssct-1]
-            self.r_fwd[:ssct-1] = self.r_fwd[dropct:old_ssct-1]
-            self.c_bwd[:ssct-1] = self.c_bwd[dropct:old_ssct-1]
-            self.c_fwd[:ssct-1] = self.c_fwd[dropct:old_ssct-1]
-
+           raise ValueError(f'negative drop count: {dropct}')
+        elif dropct > self.buffered_len():
+            raise ValueError(f'cannot drop {dropct} elements from {self.buffered_len} elements')
+        self.beginpos += dropct
+        cdef Py_ssize_t tslen = self.buffered_len()
+        cdef Py_ssize_t ssct = self.buffered_subseqct()
+        self._ts[:tslen] = self._ts[self.beginpos:self.endpos]
+        self._mu[:ssct] = self._mu[self.beginpos:self.beginpos+ssct]
+        self._invn[:ssct] = self._invn[self.beginpos:self.beginpos+ssct] 
 
     cdef append(self, double[::1] dat, Py_ssize_t dropct=0):
         if dat.shape[0] == 0:
-            raise ValueError
-        if dropct > self.tslen or dropct < 0:
-            raise ValueError
-        cdef Py_ssize_t minsz = self.tslen - dropct + dat.shape[0]
-        cdef double[::1] mu_s
-        cdef Py_ssize_t strt
-        # shift if necessary
-        if minsz <= self.ts.shape[0]:
-            if dropct > 0:
-                self.repack(dropct)
-        else:
-            self.resize(2 * minsz, dropct)
-        cdef old_tslen = self.tslen
-        cdef old_ssct = self.sseqct()
-        self.tslen += dat.shape[0]
-        self.ts[old_tslen:self.tslen] = dat
-        cdef Py_ssize_t ssct = self.sseqct()
-        cdef Py_ssize_t addct = ssct if old_ssct == 0 else dat.shape[0]
-        cdef Py_ssize_t dif_addct = addct - 1 if old_ssct == 0 else addct
-        cdef Py_ssize_t tsbeg = 0 if old_ssct == 0 else old_tslen - self.sseqlen + 1
-        cdef Py_ssize_t ssbeg = ssct - addct
-        cdef Py_ssize_t difbeg = ssct - dif_addct - 1
-        cdef Py_ssize_t difct = ssct - 1
-        windowed_mean(self.ts[tsbeg:self.tslen], self.mu[tsbeg:ssct])
-        mu_s = array(shape=(dif_addct,), itemsize=sizeof(double), format='d')
-        windowed_mean(self.ts[tsbeg:self.tslen-1], mu_s[:dif_addct])
-        mpx_step_eqns(self.ts[tsbeg:self.tslen],
-                      self.mu[ssbeg:ssct],
-                      mu_s,
-                      self.r_bwd[difbeg:difct],
-                      self.c_bwd[difbeg:difct],
-                      self.r_fwd[difbeg:difct],
-                      self.c_fwd[difbeg:difct])
+            raise ValueError('Cannot append from an empty block')
+        if not (0 < dropct < self.buffered_len()):
+            raise ValueError(f'drop count {dropct} is incompatible with buffered time series length {self.buffered_len()}')
+        cdef Py_ssize_t updsz = self.buffered_len() - dropct + dat.shape[0]
+        if updsz > self._ts.shape[0]:
+            raise ValueError(f'buffer of size {self._ts.shape[0]} is incompatible with required size {updsz}')
+        self.beginpos += dropct
+        if self._ts.shape[0] - self.beginpos < self.updsz:
+            self.repack()
+        self.endpos += dat.shape[0]
+        self._ts[self.beginpos:self.endpos] = dat[:]
+        
+        # self._mu[self.beginpos:self.beginpos+        
 
 
-cdef class AutoMProfile:
+
+cdef class MProfile:
     """ auto profile indicates that our comparisons use normalized cross correlation between 2 sections of the same
         time series
     """
@@ -268,7 +183,7 @@ cdef class AutoMProfile:
             raise ValueError
         self.minsep = minsep
         self.maxsep = maxsep
-        self.tsp = AutoParams(ts, sseqlen, ts_reserve_len)
+        self.tsp = TSParams(ts, sseqlen, ts_reserve_len)
 
 
     cdef inline Py_ssize_t bufferlen(self):
